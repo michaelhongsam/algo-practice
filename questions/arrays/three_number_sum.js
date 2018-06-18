6b8fa8ca6e0ff642e164876f3842b42bf0ae21d9
Michael Sam <michaelhongsam@gmail.com>
2018-06-18 09:52:36 -0400

    setup three number sum

---
+++ questions/arrays/three_number_sum.js
@@ -0,0 +1,6 @@
+function threeNumberSum(array, targetSum) {
+  // Write your code here.
+}
+
+// Do not edit the line below.
+exports.threeNumberSum = threeNumberSum;
---
+++ test/three_number_sum.spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert')
+var threeNumberSum = require('../questions/arrays/three_number_sum')
+
+describe('Three Number Sum', function() {
+  it('finds the target sum ...', function() {
+    assert.equal( threeNumberSum(array, targetSum) , 'heyy');
+  });
+  
+});
